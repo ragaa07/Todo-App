acfcc0bc0198d977b0f7160bff8a4b86b7d5b86d
ragaa aboelezz <mragaa07@gmail.com>
2021-07-14 14:53:37 +0200

    chore: Set App Architcture
    * I choose clean architcture with mvvm to build the app

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.domain.MyClass
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
